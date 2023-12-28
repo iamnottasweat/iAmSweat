0995c6e457c64b7d6dab579b08e23ed616a724e5
iAmNottaSweat <147358024+iamnottasweat@users.noreply.github.com>
2023-12-28 06:15:15 -0500

    misc.

---
+++ commands/utility/echo.js
@@ -0,0 +1,33 @@
+const { errorLogger, commandLogger } = require('../../logger.js');
+
+module.exports = {
+	name: 'echo',
+	description: 'Echos a message to a designated channel.',
+	cooldown: 5,
+
+	execute(message, args) {
+		try {
+			const channel = message.mentions.channels.first();
+
+			if (channel) {
+				args = args.filter((arg) => !arg.includes(channel.id));
+			}
+
+			const msg = args.join(' ');
+
+			message.delete().catch(console.error);
+
+			if (channel) {
+				channel.send(msg);
+			} else {
+				message.channel.send('Please mention a channel to echo the message.');
+				return;
+			}
+
+			commandLogger.info(`${message.guild.name} | ${message.author.username} | ECHO | ${channel.name} | ${message.createdTimestamp}`);
+		} catch (error) {
+			errorLogger.error(error);
+			message.channel.send('Sorry, I was unable to echo the message.');
+		}
+	},
+};
